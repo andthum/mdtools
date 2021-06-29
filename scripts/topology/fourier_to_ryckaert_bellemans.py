81d16f9894e232333154e825424d671fa793faee
ezavod <ezavod@gmx.de>
2021-06-29 12:04:10 +0200

    feat(test): basic test setup
    This sets up a very basic test to check Github Actions.

--- scripts/topology/fourier_to_ryckaert_bellemans.py
+++ scripts/topology/fourier_to_ryckaert_bellemans.py
@@ -32,33 +32,38 @@
 import argparse
 import numpy as np
 
-parser = argparse.ArgumentParser(
-    description = (
-        "Convert Fourier dihedral coefficients"
-        "  to Ryckaert-Bellemans dihedral coefficients."
+def calc_rb_coeff(f):
+    if len(f) != 4:
+        raise ValueError("f must have length 4")
+
+    rb = np.zeros(6)
+    rb[0] = f[1] + 0.5 * (f[0] + f[2])
+    rb[1] = 0.5 * (-f[0] + 3*f[2])
+    rb[2] = -f[1] + 4*f[3]
+    rb[3] = -2*f[2]
+    rb[4] = -4*f[3]
+    rb[5] = 0
+
+    return rb
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description = (
+            "Convert Fourier dihedral coefficients"
+            "  to Ryckaert-Bellemans dihedral coefficients."
+        )
     )
-)
-parser.add_argument(
-    '-v',
-    dest='V',
-    type=float,
-    nargs=4,
-    required=True,
-    help="The four Fourier dihedral coefficients"
-)
-args = parser.parse_args()
+    parser.add_argument(
+        '-v',
+        dest='V',
+        type=float,
+        nargs=4,
+        required=True,
+        help="The four Fourier dihedral coefficients"
+    )
+    args = parser.parse_args()
 
-f = np.array(args.V)
-if len(f) != 4:
-    raise ValueError("f must have length 4")
+    rb = calc_rb_coeff(args.V)
 
-rb = np.zeros(6)
-rb[0] = f[1] + 0.5 * (f[0] + f[2])
-rb[1] = 0.5 * (-f[0] + 3*f[2])
-rb[2] = -f[1] + 4*f[3]
-rb[3] = -2*f[2]
-rb[4] = -4*f[3]
-rb[5] = 0
-
-print("The Ryckaert-Bellemans dihedral coefficients are")
-print("   ".join([str(i) for i in rb]))
+    print("The Ryckaert-Bellemans dihedral coefficients are")
+    print("   ".join([str(i) for i in rb]))
