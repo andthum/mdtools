b429c9592cf6232ff0a6a07784663c528c7913f3
andthum <56444687+andthum@users.noreply.github.com>
2022-06-24 15:58:55 +0200

    [.mdlstyle.rb]: Create
    Style file for markdownlint.

---
+++ .mdlstyle.rb
@@ -0,0 +1,9 @@
+# markdownlint style file.
+# See
+# https://github.com/markdownlint/markdownlint/blob/master/docs/creating_styles.md
+
+all
+rule "MD007", :indent => 4  # Unordered list indentation
+rule "MD009", :br_spaces => 2  # Trailing spaces
+rule "MD013", :line_length => 72  # Line length
+exclude_rule "MD026"  # Trailing punctuation in header
