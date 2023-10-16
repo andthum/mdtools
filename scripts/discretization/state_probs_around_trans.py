c74e8f2fb8e9e9917a3154ef692e23c6080cb8ce
andthum <56444687+andthum@users.noreply.github.com>
2023-10-16 10:29:06 +0200

    [state_probs_around_trans.py]: Refactor
    * Finish the docstring.
    * Minor code refactoring.
    * Format code, comments and docstrings.

--- scripts/discretization/state_probs_around_trans.py
+++ scripts/discretization/state_probs_around_trans.py
@@ -19,50 +19,50 @@
 
 
 r"""
-Compute the probability to be in the same state as directly before or
-after a state transition as function of time passed since the transition.
-
-.. todo::
-    * Make the code actually do what the documentation says.
-    * Finish docstring.
+Calculate the probability to be in the same state as directly before or
+after a state transition as function of time passed since the
+transition.
 
 Given that at time :math:`t_0` a state transition occurred, compute the
 probability
 
     * that a compound is at time :math:`t_0 - \Delta t` in the same
       state as directly before the state transition ('prob_b_as_b').
-    * that a compound is from time :math:`t_0 - \Delta t` until
-      :math:`t_0` *continously* in the same state ('prob_b_as_b_con').
     * that a compound is at time :math:`t_0 - \Delta t` in the same
       state as directly after the state transition ('prob_b_as_a').
-
     * that a compound is at time :math:`t_0 + \Delta t` in the same
       state as directly after the state transition ('prob_a_as_a').
-    * that a compound is from time :math:`t_0` until
-      :math:`t_0 + \Delta t` *continously* in the same state
-      ('prob_a_as_a_con').
     * that a compound is at time :math:`t_0 + \Delta t` in the same
       state as directly before the state transition ('prob_a_as_b').
-
-    * that a compound is at time :math:`t_0 - \Delta t` in the same
-      state as directly before the state transition and at time
-      :math:`t_0 + \Delta t` in the same state as directly after the
-      state transition ('prob_bb_aa').
-    * that a compound is from time :math:`t_0 - \Delta t` until
-      :math:`t_0` *continously* in one state and from time :math:`t_0`
-      until :math:`t_0 + \Delta t` *continously* in another state
-      ('prob_bb_aa_con').
-
     * that a compound is at time :math:`t_0 - \Delta t` in the same
       state as at time :math:`t_0 + \Delta t` ('prob_same_sym').
 
+Options
+-------
+-f
+    Name of the file containing the discrete trajectory.  The discrete
+    trajectory must be stored as :class:`numpy.ndarray` either in a
+    binary NumPy |npy_file| or in a (compressed) NumPy |npz_archive|.
+    See :func:`mdtools.file_handler.load_dtrj` for more information
+    about the requirements for the input file.
+-o
+    Output filename.
+-b
+    First frame to read from the discrete trajectory.  Frame numbering
+    starts at zero.  Default: ``0``.
+-e
+    Last frame to read from the discrete trajectory.  This is exclusive,
+    i.e. the last frame read is actually ``END - 1``.  A value of ``-1``
+    means to read the very last frame.  Default: ``-1``.
+--every
+    Read every n-th frame from the discrete trajectory.  Default: ``1``.
+
+
 See Also
 --------
-:mod:`lig_change_at_pos_change`
-
-.. todo::
-
-    Finish docstring.
+:mod:`scripts.structure.lig_change_at_pos_change_blocks` :
+    Compare the coordination environment of reference compounds before
+    and after they have changed their position
 """
 
 
@@ -70,107 +70,84 @@ __author__ = "Andreas Thum"
 
 
 # Standard libraries
-import sys
-import os
-import warnings
 import argparse
+import os
+import sys
 from datetime import datetime, timedelta
-# Third party libraries
-import psutil
+
+# Third-party libraries
 import numpy as np
-# Local application/library specific imports
+import psutil
+
+# First-party libraries
 import mdtools as mdt
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     timer_tot = datetime.now()
     proc = psutil.Process()
-    proc.cpu_percent()  # Initiate monitoring of CPU usage
+    proc.cpu_percent()  # Initiate monitoring of CPU usage.
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description=(
-            """
-Compute the probability to be in the same state as directly before or
-after a state transition as function of time passed since the transition.
-
-Given that at time t0 a state transition occurred, compute the
-probability
-
-    * that a compound is at time t0-dt in the same state as directly
-      before the state transition ('prob_b_as_b').
-    * that a compound is from time t0-dt until t0 continously in the
-      same state ('prob_b_as_b_con').
-    * that a compound is at time t0-dt in the same state as directly
-      after the state transition ('prob_b_as_a').
-
-    * that a compound is at time t0+dt in the same state as directly
-      after the state transition ('prob_a_as_a').
-    * that a compound is from time t0 until t0+dt continously in the
-      same state ('prob_a_as_a_con').
-    * that a compound is at time t0+dt in the same state as directly
-      before the state transition ('prob_a_as_b').
-
-    * that a compound is at time t0-dt in the same state as directly
-      before the state transition and at time t0+dt in the same state as
-      directly after the state transition ('prob_bb_aa').
-    * that a compound is from time t0-dt until t0 continously in one
-      state and from time t0 until t0+dt continously in another state
-      ('prob_bb_aa_con').
-
-    * that a compound is at time t0-dt in the same state as at time
-      t0+dt ('prob_same_sym').
-"""))
-    parser.add_argument(
-        '-f',
-        dest='TRJFILE',
-        type=str,
-        required=True,
-        help="File containing the discretized trajectory stored as"
-             " numpy.ndarray in .npy format.  The array must be of shape"
-             " (n, f) where n is the number of compounds and f is the"
-             " number of frames.  The elements of the array are"
-             " interpreted as the indices of the states in which a given"
-             " compound is at a given frame."
+            "Calculate the probability to be in the same state as directly"
+            " before or after a state transition as function of time passed"
+            " since the transition.  For more information, refer to the"
+            " documentation of this script."
+        ),
     )
     parser.add_argument(
-        '-o',
-        dest='OUTFILE',
+        "-f",
+        dest="TRJFILE",
         type=str,
         required=True,
-        help="Output filename."
+        help=(
+            "File containing the discrete trajectory stored as numpy.ndarray"
+            " in the binary .npy format or as .npz archive."
+        ),
     )
     parser.add_argument(
-        '-b',
-        dest='BEGIN',
+        "-o",
+        dest="OUTFILE",
+        type=str,
+        required=True,
+        help="Output filename.",
+    )
+    parser.add_argument(
+        "-b",
+        dest="BEGIN",
         type=int,
         required=False,
         default=0,
-        help="First frame to read from the trajectory.  Frame numbering"
-             " starts at zero.  Default: %(default)s."
+        help=(
+            "First frame to read from the trajectory.  Frame numbering starts"
+            " at zero.  Default: %(default)s."
+        ),
     )
     parser.add_argument(
-        '-e',
-        dest='END',
+        "-e",
+        dest="END",
         type=int,
         required=False,
         default=-1,
-        help="Last frame to read from the trajectory.  This is"
-             " exclusive, i.e. the last frame read is actually END-1."
-             "  A value of -1 means to read the very last frame."
-             "  Default: %(default)s."
+        help=(
+            "Last frame to read from the trajectory (exclusive).  Default:"
+            " %(default)s."
+        ),
     )
     parser.add_argument(
-        '--every',
-        dest='EVERY',
+        "--every",
+        dest="EVERY",
         type=int,
         required=False,
         default=1,
-        help="Read every n-th frame from the trajectory.  Default:"
-             " %(default)s."
+        help=(
+            "Read every n-th frame from the trajectory.  Default: %(default)s."
+        ),
     )
     args = parser.parse_args()
     print(mdt.rti.run_time_info_str())
-    # TODO: Revise script
+
     print("\n")
     print("Loading trajectory...")
     timer = datetime.now()
@@ -180,28 +157,24 @@ probability
         start=args.BEGIN,
         stop=args.END,
         step=args.EVERY,
-        n_frames_tot=N_FRAMES_TOT
+        n_frames_tot=N_FRAMES_TOT,
     )
     dtrj = dtrj[:, BEGIN:END:EVERY]
-    if np.any(np.modf(dtrj)[0] != 0):
-        warnings.warn("At least one element of the discrete trajectory"
-                      " is not an integer", RuntimeWarning)
     trans_info_str = mdt.rti.dtrj_trans_info_str(dtrj)
     print("Elapsed time:         {}".format(datetime.now() - timer))
-    print("Current memory usage: {:.2f} MiB"
-          .format(proc.memory_info().rss / 2**20))
+    print("Current memory usage: {:.2f} MiB".format(mdt.rti.mem_usage(proc)))
 
     # Given that at time t0 a state transition occurred...
-    # prob_sym:    Probability that a compound is at time t0-dt in the
-    #              same state as at time t0+dt.
-    # prob_b_as_b: Probability that a compound is at time t0-dt in the
-    #              same state as directly before the state transition.
-    # prob_b_as_a: Probability that a compound is at time t0-dt in the
-    #              same state as directly after the state transition.
-    # prob_a_as_a: Probability that a compound is at time t0+dt in the
-    #              same state as directly after the state transition.
-    # prob_a_as_b: Probability that a compound is at time t0+dt in the
-    #              same state as directly before the state transition.
+    # `prob_sym`: Probability that a compound is at time t0-dt in the
+    #     same state as at time t0+dt.
+    # `prob_b_as_b`: Probability that a compound is at time t0-dt in the
+    #     same state as directly before the state transition.
+    # `prob_b_as_a`: Probability that a compound is at time t0-dt in the
+    #     same state as directly after the state transition.
+    # `prob_a_as_a`: Probability that a compound is at time t0+dt in the
+    #     same state as directly after the state transition.
+    # `prob_a_as_b`: Probability that a compound is at time t0+dt in the
+    #     same state as directly before the state transition.
     prob_sym = np.zeros(N_FRAMES // 2, dtype=np.uint32)
     norm_sym = np.zeros_like(prob_sym)
     prob_b_as_b = np.zeros(N_FRAMES - 1, dtype=np.uint32)
@@ -220,117 +193,128 @@ probability
     print("Last frame to read:     {:>8d}".format(END - 1))
     print("Read every n-th frame:  {:>8d}".format(EVERY))
     timer = datetime.now()
-    trj = mdt.rti.ProgressBar(dtrj, unit="compounds")
-    for cmp_trj in trj:  # Loop over single compound trajectories
-        # Frames directly *before* state transitions:
+    dtrj = mdt.rti.ProgressBar(dtrj, unit="compounds")
+    for cmp_trj in dtrj:  # Loop over single compound trajectories
+        # Frames directly *before* state transitions.
         trans = np.flatnonzero(np.diff(cmp_trj))
-        trans += 1  # Frames directly *after* state transitions
+        # Frames directly *after* state transitions.
+        trans += 1
         for t0 in trans:
-            # Trajectory before state transition, time reversed
-            cmp_trj_b = cmp_trj[t0 - 1::-1]
-            # Trajectory after state transition
+            # Trajectory before the state transition, time reversed.
+            cmp_trj_b = cmp_trj[t0 - 1 :: -1]
+            # Trajectory after the state transition.
             cmp_trj_a = cmp_trj[t0:]
+            # Calculate `prob_sym`.
             max_lag = min(t0, N_FRAMES - t0)
-            prob_sym[:max_lag] += (cmp_trj_b[:max_lag] ==
-                                   cmp_trj_a[:max_lag])
+            prob_sym[:max_lag] += cmp_trj_b[:max_lag] == cmp_trj_a[:max_lag]
             norm_sym[:max_lag] += 1
+            # Calculate `prob_b_as_b` and `prob_b_as_a`.
             max_lag = len(cmp_trj_b)
-            prob_b_as_b[:max_lag] += (cmp_trj_b == cmp_trj_b[0])
-            prob_b_as_a[:max_lag] += (cmp_trj_b == cmp_trj_a[0])
+            prob_b_as_b[:max_lag] += cmp_trj_b == cmp_trj_b[0]
+            prob_b_as_a[:max_lag] += cmp_trj_b == cmp_trj_a[0]
             norm_b[:max_lag] += 1
+            # Calculate `prob_a_as_a` and `prob_a_as_b`.
             max_lag = len(cmp_trj_a)
-            prob_a_as_a[:max_lag] += (cmp_trj_a == cmp_trj_a[0])
-            prob_a_as_b[:max_lag] += (cmp_trj_a == cmp_trj_b[0])
+            prob_a_as_a[:max_lag] += cmp_trj_a == cmp_trj_a[0]
+            prob_a_as_b[:max_lag] += cmp_trj_a == cmp_trj_b[0]
             norm_a[:max_lag] += 1
         # ProgressBar update:
         progress_bar_mem = proc.memory_info().rss / 2**20
-        trj.set_postfix_str("{:>7.2f}MiB".format(progress_bar_mem),
-                            refresh=False)
-    trj.close()
-    del dtrj, trj, trans, cmp_trj, cmp_trj_b, cmp_trj_a
+        dtrj.set_postfix_str(
+            "{:>7.2f}MiB".format(mdt.rti.mem_usage(proc)), refresh=False
+        )
+    dtrj.close()
     print("Elapsed time:         {}".format(datetime.now() - timer))
-    print("Current memory usage: {:.2f} MiB"
-          .format(proc.memory_info().rss / 2**20))
+    print("Current memory usage: {:.2f} MiB".format(mdt.rti.mem_usage(proc)))
+
+    prob_sym = prob_sym / norm_sym
+    prob_b_as_b = prob_b_as_b / norm_b
+    prob_b_as_a = prob_b_as_a / norm_b
+    prob_a_as_a = prob_a_as_a / norm_a
+    prob_a_as_b = prob_a_as_b / norm_a
+    del norm_sym, norm_b, norm_a
 
     print("\n")
     print("Creating output...")
     timer = datetime.now()
     lag_times = np.arange(len(prob_b_as_b), dtype=np.uint32)
-    prob_sym = prob_sym / norm_sym
-    del norm_sym
-    prob_sym = mdt.nph.extend(prob_sym,
-                              len(prob_b_as_b),
-                              fill_value=np.nan)
-    data = np.column_stack([lag_times,
-                            prob_sym,
-                            prob_b_as_b / norm_b,
-                            prob_b_as_a / norm_b,
-                            prob_a_as_a / norm_a,
-                            prob_a_as_b / norm_a])
+    prob_sym = mdt.nph.extend(prob_sym, len(prob_b_as_b), fill_value=np.nan)
+    data = np.column_stack(
+        [
+            lag_times,
+            prob_sym,
+            prob_b_as_b,
+            prob_b_as_a,
+            prob_a_as_a,
+            prob_a_as_b,
+        ]
+    )
     del prob_sym, prob_b_as_b, prob_b_as_a, prob_a_as_a, prob_a_as_b
-    del norm_b, norm_a
     header = (
-        "Probabilities to be in the same state a certain time before\n"
-        "and after a state transition.\n"
+        "Probability to be in the same state a certain time before and after\n"
+        "a state transition.\n"
         "\n"
-        "\n"
-        + trans_info_str +
-        "\n"
+        "\n" + trans_info_str + "\n"
         "\n"
         "Given that at time t0 a state transition occurred...\n"
         "The columns contain:\n"
         "  1 Lag time dt (in trajectory steps)\n"
-        "  2 prob_sym:    Probability that a compound is at time t0-dt in the\n"
-        "                 same state as at time t0+dt\n"
-        "  3 prob_b_as_b: Probability that a compound is at time t0-dt in the\n"
-        "                 same state as directly before the state transition\n"
-        "  4 prob_b_as_a: Probability that a compound is at time t0-dt in the\n"
-        "                 same state as directly after the state transition\n"
-        "  5 prob_a_as_a: Probability that a compound is at time t0+dt in the\n"
-        "                 same state as directly after the state transition\n"
-        "  6 prob_a_as_b: Probability that a compound is at time t0+dt in the\n"
-        "                 same state as directly before the state transition\n"
+        "  2 prob_sym:    Probability that a compound is at time t0-dt in\n"
+        "      the same state as at time t0+dt\n"
+        "  3 prob_b_as_b: Probability that a compound is at time t0-dt in\n"
+        "      the same state as directly before the state transition\n"
+        "  4 prob_b_as_a: Probability that a compound is at time t0-dt in\n"
+        "      the same state as directly after the state transition\n"
+        "  5 prob_a_as_a: Probability that a compound is at time t0+dt in\n"
+        "      the same state as directly after the state transition\n"
+        "  6 prob_a_as_b: Probability that a compound is at time t0+dt in\n"
+        "      the same state as directly before the state transition\n"
         "\n"
         "Column number:\n"
-        "{:>14d} {:>16d} {:>16d} {:>16d} {:>16d} {:>16d}\n"
-        "{:>14s} {:>16s} {:>16s} {:>16s} {:>16s} {:>16s}"
-        .format(1, 2, 3, 4, 5, 6,
-                "dt", "prob_sym",
-                "prob_b_as_b", "prob_b_as_a",
-                "prob_a_as_a", "prob_a_as_b")
     )
+    header += "{:>14d}".format(1)
+    for i in range(2, data.shape[-1] + 1):
+        header += " {:>16d}".format(i)
     mdt.fh.savetxt(args.OUTFILE, data, header=header)
     print("Created {}".format(args.OUTFILE))
     print("Elapsed time:         {}".format(datetime.now() - timer))
-    print("Current memory usage: {:.2f} MiB"
-          .format(proc.memory_info().rss / 2**20))
+    print("Current memory usage: {:.2f} MiB".format(mdt.rti.mem_usage(proc)))
 
     print("\n")
-    print("Creating output for consistency...")
+    print("Checking output for consistency...")
     timer = datetime.now()
-    names = ("prob_sym",
-             "prob_b_as_b", "prob_b_as_a",
-             "prob_a_as_a", "prob_a_as_b")
+    names = (
+        "prob_sym",
+        "prob_b_as_b",
+        "prob_b_as_a",
+        "prob_a_as_a",
+        "prob_a_as_b",
+    )
     first_values = (0, 1, 0, 1, 0)
     for i, prob in enumerate(data.T[1:]):
         if not np.isclose(prob[0], first_values[i]):
-            raise ValueError("The first element of '{}' ({}) is not {}"
-                             .format(names[i], prob[0], first_values[i]))
+            raise ValueError(
+                "The first element of '{}' ({}) is not"
+                " {}".format(names[i], prob[0], first_values[i])
+            )
         if np.any(prob > 1):
-            raise ValueError("At least one element of '{}' is greater"
-                             " than one.".format(names[i]))
+            raise ValueError(
+                "At least one element of '{}' is greater than"
+                " one.".format(names[i])
+            )
         if np.any(prob < 0):
-            raise ValueError("At least one element of '{}' is less than"
-                             " zero.".format(names[i]))
+            raise ValueError(
+                "At least one element of '{}' is less than"
+                " zero.".format(names[i])
+            )
+    print("Created {}".format(args.OUTFILE))
     print("Elapsed time:         {}".format(datetime.now() - timer))
-    print("Current memory usage: {:.2f} MiB"
-          .format(proc.memory_info().rss / 2**20))
+    print("Current memory usage: {:.2f} MiB".format(mdt.rti.mem_usage(proc)))
 
     print("\n")
     print("{} done".format(os.path.basename(sys.argv[0])))
     print("Totally elapsed time: {}".format(datetime.now() - timer_tot))
-    print("CPU time:             {}"
-          .format(timedelta(seconds=sum(proc.cpu_times()[:4]))))
+    _cpu_time = timedelta(seconds=sum(proc.cpu_times()[:4]))
+    print("CPU time:             {}".format(_cpu_time))
     print("CPU usage:            {:.2f} %".format(proc.cpu_percent()))
-    print("Current memory usage: {:.2f} MiB"
-          .format(proc.memory_info().rss / 2**20))
+    print("Current memory usage: {:.2f} MiB".format(mdt.rti.mem_usage(proc)))
