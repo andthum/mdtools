7b33b3ea75eda2f8df9bdfadbd52c26ca2fb92cb
andthum <56444687+andthum@users.noreply.github.com>
2025-02-23 18:15:49 +0100

    [conf.py]: Linkcheck: Extend List of Ignored URLs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -481,6 +481,7 @@ linkcheck_ignore = [
     r"https://www\.cabdirect\.org/cabdirect/abstract/19272700028",
     r"https://doi\.org/10\.1002/9781118033005",
     r"https://doi\.org/10\.1021/acs\.jctc\.2c00327",
+    r"https://doi\.org/10\.1063/1\.1320826",
     r"https://doi\.org/10\.1063/5\.0008316",
     r"https://doi\.org/10\.1073/pnas\.1900239116",
     r"https://doi\.org/10\.1080/00031305\.2017\.1356374",
@@ -489,6 +490,7 @@ linkcheck_ignore = [
     r"https://doi\.org/10\.1103/PhysRev\.136\.A405",
     r"https://doi\.org/10\.1103/PhysRevB\.74\.184430",
     r"https://doi\.org/10\.1103/PhysRevLett\.98\.227802",
+    r"https://doi\.org/10\.3233/DS-190026",
     # 418 Client Error: Unknown for url.
     r"https://doi\.org/10\.1109/TSMC\.1979\.4310076",
 ]
